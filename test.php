f4e76c3c2c31e75448a28187caddeb990d205b9e
CY0290 <65933458+locklen860118@users.noreply.github.com>
2020-09-01 11:00:01 +0800

    one week weather data complete

--- test.php
+++ test.php
@@ -6,8 +6,8 @@
     $ch = curl_init();
 
     // 從檔案中讀取資料到PHP變數 
-    curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-1B423024-C23F-44A7-9E83-AB17227AC4A3&limit=3&format=JSON&locationName=%E6%96%B0%E7%AB%B9%E7%B8%A3,%E6%96%B0%E7%AB%B9%E5%B8%82,%E8%8B%97%E6%A0%97%E7%B8%A3");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-1B423024-C23F-44A7-9E83-AB17227AC4A3&elementName=,MinT,MaxT,Wx");//一週天氣
+    curl_setopt($ch, CURLOPT_URL, "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-089?Authorization=CWB-1B423024-C23F-44A7-9E83-AB17227AC4A3&format=JSON&elementName=PoP12h,T,WeatherDescription");//兩天天氣
     curl_setopt($ch, CURLOPT_HEADER, 0); 
     
     $pageContent = curl_exec($ch);
@@ -23,16 +23,16 @@
     // var_dump($json_string); 
     // var_dump ($data); 
     //  print_r($data);
-    $num=0;
-    foreach($data["records"]["location"] as $i){
-        echo $i["locationName"]."<br>";
-        echo $i["weatherElement"][0]["time"][0]["startTime"]."<br>";
-        echo $i["weatherElement"][0]["time"][0]["endTime"]."<br>";
-        echo $i["weatherElement"][0]["time"][0]["parameter"]["parameterName"]."<br>";
-        for($cnt=0;$cnt<3;$cnt++){
-            echo $i["weatherElement"][0]["time"][$cnt]["parameter"]["parameterName"]."<br>";
-        }
-    } 
+ 
+    // foreach($data["records"]["locations"][0]["location"] as $i){
+    //         echo $i["locationName"]."<br>";
+    //         for($k=0;$k<14;$k++){
+    //                 echo $i["weatherElement"][0]["time"][$k]["elementValue"][0]["value"]."   ,";
+    //                 echo $i["weatherElement"][1]["time"][$k]["elementValue"][0]["value"]."   ,";
+    //                 echo $i["weatherElement"][2]["time"][$k]["elementValue"][0]["value"]."   ,".$i["weatherElement"][0]["time"][$k]["startTime"]."<br>";
+    //         }
+    //         echo "<br>";
+    // } 
     
 
    
